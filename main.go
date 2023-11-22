137fc4adcc5c938aa6ca15fe05fefcf5ef49a125
PullolilEzequiel <ezequielkarottupullolil@gmail.com>
2023-11-22 03:01:08 -0300

    (refact) Reduce the code in main func

--- main.go
+++ main.go
@@ -2,32 +2,20 @@ package main
 
 import (
 	"flag"
-	"fmt"
 )
 
 func main() {
 	var url URL
 	var method http_method
 	var methods []http_method
-	complexFlag := flag.Bool("complex", false, "Allows complex requests")
+	complexFlag := flag.Bool("complex", false, "Enable complex requests")
 
 	flag.Parse()
 	if *complexFlag {
 		url, methods = complexArgumentParser()
+		makeComplexRequest(url, methods)
 	} else {
 		url, method = simpleArgumentParser()
-	}
-	if len(methods) == 0 {
-		resp, err := makeRequest(url, method)
-		if err == nil {
-			fmt.Print(resp)
-		}
-	} else {
-		for _, m := range methods {
-			resp, err := makeRequest(url, m)
-			if err == nil {
-				fmt.Print(resp)
-			}
-		}
+		makeRequest(url, method)
 	}
 }
