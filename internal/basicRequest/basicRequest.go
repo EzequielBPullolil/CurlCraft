e01c77ff53682201ca564bac98fe004a272d1c4c
PullolilEzequiel <ezequielkarottupullolil@gmail.com>
2023-11-28 21:24:15 -0300

    (feat) Now the print Response procedure prints only the headers have information

--- internal/basicRequest/basicRequest.go
+++ internal/basicRequest/basicRequest.go
@@ -1,13 +1,9 @@
 package basicrequest
 
 import (
-	"fmt"
-
 	"github.com/EzequielK-source/CurlCraft/internal"
 )
 
 func Request(url string, method string) {
 	internal.MakeRequest(url, method)
-	fmt.Println(url)
-	fmt.Println(method)
 }
