2589ed2381dd2dc945ec401cec4596e70df1b0f7
PullolilEzequiel <ezequielkarottupullolil@gmail.com>
2023-12-04 06:02:27 -0300

    (files) removed unused file
    - Remove unused method

--- internal/method_interface.go
+++ internal/method_interface.go
@@ -19,7 +19,3 @@ func IsHttpMethod(e string) bool {
 	e = strings.ToUpper(e)
 	return e == POST || e == GET || e == DELETE || e == PUT || e == PATCH
 }
-
-func isURL(e string) bool {
-	return strings.HasPrefix(e, "http")
-}
